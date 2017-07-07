e271c134d9abe26493efbec4c9db33dd94f0f989
anonymoussc <50c5ac69@opayq.com>
2017-07-07 16:03:30 +0700

    Master/Detail

--- components/app/app.component.ts
+++ components/app/app.component.ts
@@ -6,5 +6,37 @@ import {Component} from '@angular/core';
   styleUrls  : ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signoir';
+  title = 'Tour of heroes';
+  //hero = 'Romanov';
+
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'victoria'
+  // }
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
 }
+
+export class Hero {
+  id : number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
