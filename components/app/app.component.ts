ea1077eb8c0115ce5b4eb76bf0e709a0ea659d6d
anonymoussc <50c5ac69@opayq.com>
2017-07-10 03:35:15 +0700

    Updated

--- components/app/app.component.ts
+++ components/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector   : 'app-root',
@@ -7,36 +8,30 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of heroes';
-  //hero = 'Romanov';
+  // hero = 'Romanov';
 
   // hero : Hero = {
   //   id: 1,
   //   name: 'victoria'
   // }
+  heroes = HEROES;
 
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-  heroes = HEROES;
-}
-
-export class Hero {
-  id : number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
 ];
